6241a5d776e69128169e885bf763cee65d34fb5d
Netflixxp <61532011+Netflixxp@users.noreply.github.com>
2021-06-15 07:21:32 +0800

    do

--- diy-part2.sh
+++ diy-part2.sh
@@ -123,7 +123,7 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/lang\/golang\/golang\-package\.mk/include \$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang\-package\.mk/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHREPO/PKG_SOURCE_URL:=https:\/\/github\.com/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload\.github\.com/g' {}
-svn co https://github.com/Lienol/openwrt-packages/trunk/utils/dockerd
-svn co https://github.com/Lienol/openwrt-packages/trunk/utils/docker
+#svn co https://github.com/Lienol/openwrt-packages/trunk/utils/dockerd
+#svn co https://github.com/Lienol/openwrt-packages/trunk/utils/docker
 ./scripts/feeds update -a
 ./scripts/feeds install -a
